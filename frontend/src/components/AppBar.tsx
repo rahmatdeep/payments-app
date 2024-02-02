2f18b05b79d65d85055bb35256a19236c0cd07d9
rahmatdeep <rahmatdeep@gmail.com>
2024-02-02 12:55:37 +0530

    populate dashboard page with user info

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -1,15 +1,15 @@
-export const Appbar = () => {
+export const Appbar = ({name}) => {
     return <div className="shadow h-14 flex justify-between">
         <div className="flex flex-col justify-center h-full ml-4">
-            PayTM App
+            Payments App
         </div>
         <div className="flex">
             <div className="flex flex-col justify-center h-full mr-4">
-                Hello
+                Hello {`${name[0].toUpperCase()}${name.slice(1)}`}
             </div>
             <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
                 <div className="flex flex-col justify-center h-full text-xl">
-                    U
+                    {name[0].toUpperCase()}
                 </div>
             </div>
         </div>
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,14 +1,33 @@
+import { useEffect, useState } from "react";
 import { Appbar } from "../components/AppBar";
 import { Balance } from "../components/Balance";
 import { Users } from "../components/Users";
+import axios from "axios";
 
 export function Dashboard() {
+  const [balance, setBalance] = useState(0)
+  const [name, setName] = useState("")
+
+  useEffect( ()=>{
+    async function getInfo() {
+      const response = await axios.get("http://localhost:3000/api/v1/user/user-info", {
+      headers:{
+        Authorization: `Bearer ${localStorage.getItem('token')}`
+      }
+    })
+    setBalance(response.data.account.balance)
+    setName(response.data.user.firstName)
+    }
+    getInfo()
+    
+  },[])
+
   return (
     <div>
-      <Appbar />
+      <Appbar name={name}/>
       <div className="m-8">
-        <Balance value={"10,000"} />
-        <Users />
+        <Balance value={balance} />
+        <Users/>
       </div>
     </div>
   );
