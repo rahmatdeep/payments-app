84e62cc641ff1272ad301e2899ed98ebe998f143
rahmatdeep <rahmatdeep@gmail.com>
2024-02-03 14:36:46 +0530

    fix styling of Send Money Page

--- frontend/src/pages/SendMoney.tsx
+++ frontend/src/pages/SendMoney.tsx
@@ -10,11 +10,11 @@ export const SendMoney = () => {
   return (
     <div className="flex justify-center h-screen bg-gray-100">
       <div className="h-full flex flex-col justify-center">
-        <div className="border h-min text-card-foreground max-w-md p-4 space-y-8 w-96 bg-white shadow-lg rounded-lg">
-          <div className="flex flex-col space-y-1.5 p-6">
+        <div className="border h-min max-w-md p-4  w-96 bg-white shadow-lg rounded-lg">
+          <div className="flex flex-col px-4 py-4">
             <h2 className="text-3xl font-bold text-center">Send Money</h2>
           </div>
-          <div className="p-6">
+          <div className="px-6 py-4">
             <div className="flex items-center space-x-4">
               <div className="w-12 h-12 rounded-full bg-green-500 flex items-center justify-center">
                 <span className="text-2xl text-white">
@@ -26,9 +26,9 @@ export const SendMoney = () => {
               )}`}</h3>
             </div>
             <div className="space-y-4">
-              <div className="space-y-2">
+              <div className="mt-3">
                 <label
-                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  className="pl-1 text-md font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-slate-500"
                   htmlFor="amount"
                 >
                   Amount (in Rs)
@@ -44,24 +44,24 @@ export const SendMoney = () => {
                 />
               </div>
               <button
-                onClick={async() => {
-                  try{const response = await axios.post(
-                    "http://localhost:3000/api/v1/account/transfer",
-                    {
-                      to: id,
-                      amount,
-                    },
-                    {
-                      headers: {
-                        Authorization:
-                          "Bearer " + localStorage.getItem("token"),
+                onClick={async () => {
+                  try {
+                    const response = await axios.post(
+                      "http://localhost:3000/api/v1/account/transfer",
+                      {
+                        to: id,
+                        amount,
                       },
-                    }
-                  );}catch(e){
+                      {
+                        headers: {
+                          Authorization:
+                            "Bearer " + localStorage.getItem("token"),
+                        },
+                      }
+                    );
+                  } catch (e) {
                     alert(e.response.data.msg);
-                    
                   }
-                  
                 }}
                 className="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white"
               >
